2558d7bd070dc9226f40e6e01f856767a80ce269
Elton <eltonsa75@hotmail.com>
2020-12-09 13:09:13 -0300

    Composição com Promise

---
+++ funcional/composicao_2.js
@@ -0,0 +1,48 @@
+
+function composicao(...fns) {
+    return function (valor) {
+        return fns.reduce(async (acc, fn) => {
+            if(Promise.resolve(acc) === acc) {
+                return fn(await acc)
+            } else {
+                return fn(acc)
+            }
+        }, valor)
+    }
+}
+
+function gritar(texto){
+    return texto.toUpperCase()
+}
+
+function enfatizar(texto){
+    return `${texto}!!!!`
+}
+
+function tornarLento(texto){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(texto.split('').join(' '))
+        }, 3000)
+    })
+}
+
+
+// Composição de Funções #1
+const exagerado = composicao (
+    gritar,
+    enfatizar,
+    tornarLento
+)
+
+// Composição de Funções #2
+const umPoucoMenosExagerado = composicao (
+    gritar,
+    enfatizar
+)
+
+exagerado('PARA')
+.then(console.log)
+umPoucoMenosExagerado('Cuidado com o buraco!!!')
+.then(console.log)
+
